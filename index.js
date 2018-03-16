c9ef9ddd43baec183d6fa0aa48421a0071e4db1b
Omar Guerrero <guerrero9725@gmail.com>
2018-03-15 21:08:26 -0500

    feat: Added github credentials

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var chalk = require("chalk");
 var clear = require("clear");
 var figlet = require("figlet");
 var files = require("./lib/files");
+var inquirer = require("./lib/inquirer");
 
 clear();
 console.log(
@@ -9,3 +10,15 @@ console.log(
         figlet.textSync('Ginit', {horizontalLayout: 'full'})
     )
 );
+
+if (files.directoryExits('.git')) {
+    console.log(chalk.red('Alredy a git repository!'));
+    process.exit();
+}
+
+const run = async () => {
+    const credentials = await inquirer.askGithubCredentials();
+    console.log(credentials);
+}
+
+run();
